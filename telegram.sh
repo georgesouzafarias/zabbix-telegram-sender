161942d39f9f73ac55a1318eeaab1f132a982b25
George Souza Farias <georgesouzafarias@gmail.com>
2015-03-17 22:42:08 -0400

    Second Commit
    In this commit was added the comments about the code.

--- telegram.sh
+++ telegram.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/expect
 
 
-
+#variable for destination.
+#Tip, replace "spaces" for "_" on the names.
 set TO "<Destination>"
-set MSG [lrange $argv 1 end]
+#variable for message, from 3rd argument to latest argument.
+set MSG [lrange $argv 2 end]
+#variable for timeout
 set timeout 20
+#variable for pattern - it isn't working.
 set PATTERN "<Pattern>"
-
-puts PATTERN
 
 
 proc TryConnection { } {
@@ -17,20 +19,25 @@ proc TryConnection { } {
          }
 }
 
-
+#Here's where the expect active the process
 spawn telegram -W -k /opt/tg/tg-server.pub -c /etc/telegram-cli/config
 
+
+#This is the conditional for test the telegram's connection.
 if {[TryConnection] == true} {
+        #Here the command that send the message.
         send "msg $TO $MSG\r"
         sleep 5
 
 } else {
         set ANSWER false
+        #Here, start the loop that is going to try the connection again.
         while { $ANSWER == false } {
                 send "dialog_list\r"
                 sleep 10
                 expect PATTERN { set ANSWER true }
 }
+        #Here the command that send the message.
         send "msg $TO $MSG\r"
 
 }
@@ -40,5 +47,3 @@ expect {
         timeout {send "safe_quit"; exit}
 
 }
-
-
